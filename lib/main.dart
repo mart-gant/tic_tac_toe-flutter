0059ea52359394a3b77bb43463a6d2443f62546d
mart-gant <martgant@gmail.com>
2024-09-14 12:21:07 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,160 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(TicTacToeApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class TicTacToeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tic Tac Toe',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TicTacToeHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TicTacToeHomePage extends StatefulWidget {
+  const TicTacToeHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TicTacToeHomePageState createState() => _TicTacToeHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TicTacToeHomePageState extends State<TicTacToeHomePage> {
+  static const int boardSize = 3;
+  late List<List<String>> board;
+  late String currentPlayer;
+  late bool gameEnd;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _initializeGame();
+  }
+
+  void _initializeGame() {
+    board = List.generate(boardSize, (_) => List.generate(boardSize, (_) => ''));
+    currentPlayer = 'X';
+    gameEnd = false;
+  }
+
+  void _handleTap(int row, int col) {
+    if (board[row][col].isEmpty && !gameEnd) {
+      setState(() {
+        board[row][col] = currentPlayer;
+        if (_checkWinner(row, col)) {
+          gameEnd = true;
+          _showWinnerDialog(currentPlayer);
+        } else if (_isBoardFull()) {
+          gameEnd = true;
+          _showDrawDialog();
+        } else {
+          currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+        }
+      });
+    }
+  }
+
+  bool _checkWinner(int row, int col) {
+    // Check row
+    if (board[row].every((cell) => cell == currentPlayer)) return true;
+    // Check column
+    if (board.every((r) => r[col] == currentPlayer)) return true;
+    // Check diagonals
+    if (row == col && List.generate(boardSize, (i) => board[i][i]).every((cell) => cell == currentPlayer)) return true;
+    if (row + col == boardSize - 1 && List.generate(boardSize, (i) => board[i][boardSize - 1 - i]).every((cell) => cell == currentPlayer)) return true;
+    return false;
+  }
+
+  bool _isBoardFull() {
+    for (var row in board) {
+      for (var cell in row) {
+        if (cell.isEmpty) return false;
+      }
+    }
+    return true;
+  }
+
+  void _showWinnerDialog(String winner) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('Game Over'),
+        content: Text('$winner wins!'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              setState(() {
+                _initializeGame();
+              });
+            },
+            child: Text('Play Again'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showDrawDialog() {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('Game Over'),
+        content: Text('It\'s a draw!'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              setState(() {
+                _initializeGame();
+              });
+            },
+            child: Text('Play Again'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Tic Tac Toe'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          for (int row = 0; row < boardSize; row++)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (int col = 0; col < boardSize; col++)
+                  GestureDetector(
+                    onTap: () => _handleTap(row, col),
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                      ),
+                      child: Center(
+                        child: Text(
+                          board[row][col],
+                          style: TextStyle(fontSize: 32),
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
